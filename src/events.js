c35a817676614f16b5b9d2a840a50aeecbf1fc89
Moon <espeniel69@gmail.com>
2026-05-17 17:11:08 +0900

    feat: event name constants

---
+++ src/events.js
@@ -0,0 +1,12 @@
+export const EVENTS = {
+  ENEMY_REACHED_BASE: 'enemy:reached-base',
+  ENEMY_KILLED: 'enemy:killed',
+  MAGE_SUMMONED: 'mage:summoned',
+  MAGE_MERGED: 'mage:merged',
+  MAGE_ATTACKED: 'mage:attacked',
+  WAVE_STARTED: 'wave:started',
+  WAVE_ENDED: 'wave:ended',
+  GAME_OVER: 'game:over',
+  GOLD_CHANGED: 'gold:changed',
+  HP_CHANGED: 'hp:changed',
+};
